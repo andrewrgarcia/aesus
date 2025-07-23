25b17d26ba6a61dac8c141b18f40e0d62013536a
Andrew Garcia <garcia.gtr@gmail.com>
2025-07-23 13:32:30 -0500

    modularization long text

---
+++ src/wordlist.rs
@@ -0,0 +1,8 @@
+pub const DICEWARE_WORDS: &[&str] = &[
+    "apple", "banana", "cloud", "delta", "echo", "flame", "king", "honey", "iron", "jelly",
+    "santa", "lemon", "magic", "neon", "orbit", "pearl", "quest", "magnetic", "solar", "toast",
+    "ultra", "vapor", "whale", "acid", "pancake", "penguin", "amber", "beacon", "crane", "drift",
+    "ember", "frost", "grove", "hazel", "icicle", "jungle", "karma", "lunar", "mango", "nova",
+    "oxide", "petal", "quartz", "raven", "torch", "hail", "unity", "ice", "moon", "xerox",
+    "nightcore", "hangar", "prophet", "blaze", "absinthe", "hole", "ember", "forge", "black", "halo",
+];
